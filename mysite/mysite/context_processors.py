4b84ac3262a6ebdf8adc3f41d911fb18382aa8fe
Jim Lin <jimlintw922@gmail.com>
2016-08-18 11:05:06 +0800

    rename with sitemap

--- mysite/mysite/context_processors.py
+++ mysite/mysite/context_processors.py
@@ -1,13 +1,15 @@
 from django.apps import apps
 
 
-def dashboards(request):
+def sitemap(request):
     dashboards = []
     for config in apps.get_app_configs():
         if hasattr(config, 'menu'):
             if 'panel' in config.menu:
                 # a panel app
-                if any(d['name'] == config.menu['dashboard'] for d in dashboards):
+                if any(d['name'] == config.menu['dashboard']
+                       for d in dashboards):
+
                     # parent dashboard already exists TODO
                     pass
                 else:
@@ -15,11 +17,13 @@ def dashboards(request):
                                        'panels': [{'name': config.name}]})
             else:
                 # a dashboard app
-                if any(d['name'] == config.menu['dashboard'] for d in dashboards):
+                if any(d['name'] == config.menu['dashboard']
+                       for d in dashboards):
+
                     # dashboard already exists
                     pass
                 else:
                     dashboards.append({'name': config.name, 'panels': []})
 
-    menu = {'menu': dashboards}
-    return menu
+    sitemap = {'sitemap': dashboards}
+    return sitemap
