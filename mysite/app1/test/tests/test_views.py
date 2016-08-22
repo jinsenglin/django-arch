aa081e9ba69f0809904be1ed38763f77d4369206
Jim Lin <jimlintw922@gmail.com>
2016-08-22 17:38:14 +0800

    update test_views.py to test en locale

--- mysite/app1/test/tests/test_views.py
+++ mysite/app1/test/tests/test_views.py
@@ -1,4 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib.auth.models import User
 from django.test import Client
+from django.test import override_settings
+
+import pytest
 
 
 def test_index():
@@ -6,3 +12,21 @@ def test_index():
     response = client.get('/app1/')
 
     assert response.status_code == 200
+
+
+@override_settings(LANGUAGE_CODE='en')
+@pytest.mark.django_db(transaction=False)
+def test_index_for_authenticated_en_user():
+    User.objects.create_user(username='ad02',
+                             password='p@ssw0rd',
+                             first_name='02',
+                             last_name='AD')
+
+    client = Client()
+    authenticated = client.login(username='ad02', password='p@ssw0rd')
+
+    assert authenticated
+
+    response = client.get('/app1/')
+
+    assert u'Application 1' in response.content.decode('utf8')
