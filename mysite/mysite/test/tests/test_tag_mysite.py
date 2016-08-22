834a9a4f1de211beea41067e7ba6ec9aa1e000e7
Jim Lin <jimlintw922@gmail.com>
2016-08-22 16:16:50 +0800

    seperate test cases for anonymou and authenticated users

--- mysite/mysite/test/tests/test_tag_mysite.py
+++ mysite/mysite/test/tests/test_tag_mysite.py
@@ -1,9 +1,13 @@
-from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import AnonymousUser, User
 from django.template import Template, Context
 from django.test import RequestFactory
 
+import pytest
 
-def test_get_current_role():
+from mysite import constants
+
+
+def test_get_current_role_for_anonymouse_user():
     user = AnonymousUser()
     factory = RequestFactory()
     request = factory.get('/')
@@ -14,3 +18,20 @@ def test_get_current_role():
     rendered = template.render(context)
 
     assert rendered == u''
+
+
+@pytest.mark.django_db(transaction=False)
+def test_get_current_role_for_authenticated_user():
+    user = User.objects.create_user(username='ad02',
+                                    password='p@ssw0rd',
+                                    first_name='02',
+                                    last_name=constants.ROLE_SUPER_ADMIN)
+    factory = RequestFactory()
+    request = factory.get('/')
+    request.user = user
+    context = Context({'request': request})
+
+    template = Template("{% load mysite %}{% get_current_role %}")
+    rendered = template.render(context)
+
+    assert rendered == user.last_name
