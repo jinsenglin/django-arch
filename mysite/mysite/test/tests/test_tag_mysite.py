f81ad61d899c1a99025e00270916e9bcfdc8e7a9
Jim Lin <jimlintw922@gmail.com>
2016-08-22 13:12:48 +0800

    update test_tag_mysite.py

--- mysite/mysite/test/tests/test_tag_mysite.py
+++ mysite/mysite/test/tests/test_tag_mysite.py
@@ -1,8 +1,15 @@
 from django.template import Template, Context
+from django.test import RequestFactory
 
 
 def test_get_current_role():
+    factory = RequestFactory()
+    request = factory.get('/')
+    context = Context({'request': request})
+
     template = Template("{% load mysite %}{% get_current_role %}")
-    rendered = template.render(Context({'request': None}))
+    rendered = template.render(context)
+
+    # TODO : replace with real role, e.g., request.user.role
 
     assert rendered == u'SA'
