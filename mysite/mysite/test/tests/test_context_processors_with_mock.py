f0c4eb9810a9bf258f286e8da725e74b950bdeeb
Jim Lin <jimlintw922@gmail.com>
2016-08-24 17:40:15 +0800

    update test_context_processors_with_mock.py

--- mysite/mysite/test/tests/test_context_processors_with_mock.py
+++ mysite/mysite/test/tests/test_context_processors_with_mock.py
@@ -1,13 +1,29 @@
 from django.contrib.auth.models import AnonymousUser, User
 from django.test import RequestFactory
+from django.apps import AppConfig
 
 import pytest
 from mock import patch
 
 from mysite import context_processors
+from mysite import constants
 
 
-@patch('mysite.context_processors.apps.get_app_configs')
+class X(AppConfig):
+    name = 'app1'
+    verbose_name = 'App 1'
+    menu = {'dashboard': name,
+            'name': verbose_name,
+            'order': 10,
+            'path': name,
+            'scope': [constants.ROLE_SUPER_ADMIN,
+                      constants.ROLE_SYSTEM_ADMIN,
+                      constants.ROLE_PROJECT_ADMIN,
+                      constants.ROLE_PROJECT_USER,
+                      constants.ROLE_AUDIT_USER]}
+
+
+@patch('mysite.context_processors.apps.get_app_configs', return_value=[X])
 def test_sitemap_for_anonymous_user(configs):
     user = AnonymousUser()
     factory = RequestFactory()
@@ -26,7 +42,7 @@ def test_sitemap_for_anonymous_user(configs):
     assert len(sitemap['sitemap']) == 0
 
 
-@patch('mysite.context_processors.apps.get_app_configs')
+@patch('mysite.context_processors.apps.get_app_configs', return_value=[X])
 @pytest.mark.django_db(transaction=False)
 def test_sitemap_for_authenticated_user(configs):
     user = User.objects.create(username='ad02',
@@ -41,9 +57,9 @@ def test_sitemap_for_authenticated_user(configs):
     # test the first hit
     sitemap = context_processors.sitemap(request)
     assert 'sitemap' in sitemap
-    assert len(sitemap['sitemap']) == 0
+    assert len(sitemap['sitemap']) == 1
 
     # test the cached
     sitemap = context_processors.sitemap(request)
     assert 'sitemap' in sitemap
-    assert len(sitemap['sitemap']) == 0
+    assert len(sitemap['sitemap']) == 1
