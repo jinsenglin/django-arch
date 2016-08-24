30581e4cb402d21c4dd8946cb206a9e1a57e0fe5
Jim Lin <jimlintw922@gmail.com>
2016-08-24 17:14:03 +0800

    update test_funcs_with_fixture.py

--- mysite/mysite/test/tests/test_funcs_with_fixture.py
+++ mysite/mysite/test/tests/test_funcs_with_fixture.py
@@ -15,13 +15,72 @@ class MySiteTest(StaticLiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_login(self):
+    def test_login_ad(self):
         self.browser.get(self.live_server_url + '/admin/login/')
 
-        self.browser.find_element_by_id('id_username').send_keys("cclin")
+        self.browser.find_element_by_id('id_username').send_keys("ad01")
         self.browser.find_element_by_id("id_password").send_keys('p@ssw0rd')
         self.browser.find_element_by_css_selector(
             'form input[type="submit"]').click()
 
         self.assertEquals(u'Site administration | Django site admin',
                           self.browser.title)
+
+        self.browser.get(self.live_server_url)
+        self.assertEquals(u'mysite', self.browser.title)
+
+    def test_login_sa(self):
+        self.browser.get(self.live_server_url + '/admin/login/')
+
+        self.browser.find_element_by_id('id_username').send_keys("sa01")
+        self.browser.find_element_by_id("id_password").send_keys('p@ssw0rd')
+        self.browser.find_element_by_css_selector(
+            'form input[type="submit"]').click()
+
+        self.assertEquals(u'Site administration | Django site admin',
+                          self.browser.title)
+
+        self.browser.get(self.live_server_url)
+        self.assertEquals(u'mysite', self.browser.title)
+
+    def test_login_pa(self):
+        self.browser.get(self.live_server_url + '/admin/login/')
+
+        self.browser.find_element_by_id('id_username').send_keys("pa01")
+        self.browser.find_element_by_id("id_password").send_keys('p@ssw0rd')
+        self.browser.find_element_by_css_selector(
+            'form input[type="submit"]').click()
+
+        self.assertEquals(u'Site administration | Django site admin',
+                          self.browser.title)
+
+        self.browser.get(self.live_server_url)
+        self.assertEquals(u'mysite', self.browser.title)
+
+    def test_login_pu(self):
+        self.browser.get(self.live_server_url + '/admin/login/')
+
+        self.browser.find_element_by_id('id_username').send_keys("pu01")
+        self.browser.find_element_by_id("id_password").send_keys('p@ssw0rd')
+        self.browser.find_element_by_css_selector(
+            'form input[type="submit"]').click()
+
+        self.assertEquals(u'Site administration | Django site admin',
+                          self.browser.title)
+
+        self.browser.get(self.live_server_url)
+        self.assertEquals(u'mysite', self.browser.title)
+
+    def test_login_au(self):
+        self.browser.get(self.live_server_url + '/admin/login/')
+
+        self.browser.find_element_by_id('id_username').send_keys("au01")
+        self.browser.find_element_by_id("id_password").send_keys('p@ssw0rd')
+        self.browser.find_element_by_css_selector(
+            'form input[type="submit"]').click()
+
+        self.assertEquals(u'Site administration | Django site admin',
+                          self.browser.title)
+
+        self.browser.get(self.live_server_url)
+        self.assertEquals(u'mysite', self.browser.title)
